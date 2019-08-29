19dd84edad35213fef3d399ff27b9f6de7a87f06
ethan.griffin <ethan.griffin@mu-c1-07.mullen.pvt.k12.co.us>
2019-08-29 12:21:57 -0600

    made classes

---
+++ Maze Generator/src/Generator.java
@@ -0,0 +1,9 @@
+
+public class Generator
+	{
+
+		int mazeSize = 5;
+		
+		
+		
+	}
---
+++ Maze Generator/src/Room.java
@@ -0,0 +1,14 @@
+
+public class Room
+	{
+
+		private int xPos;
+		private int yPos;
+		private String[] doors = new String[4];
+		private boolean hasBeenVisited;
+		
+		
+		
+		
+		
+	}

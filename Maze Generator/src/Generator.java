88fe090095fd687371e36e9804fd64a5fcf9b19b
ethan.griffin <ethan.griffin@mu-c1-07.mullen.pvt.k12.co.us>
2019-09-03 11:55:58 -0600

    started

--- Maze Generator/src/Generator.java
+++ Maze Generator/src/Generator.java
@@ -3,7 +3,17 @@ public class Generator
 	{
 
 		int mazeSize = 5;
-		
-		
-		
+		Room[][] map = new Room[mazeSize][mazeSize];
+
+		for(
+		int x = 0;x>mazeSize;x++)
+			{
+				for (int y = 0; y > mazeSize; y++)
+					{
+
+						map[x][y] = new Room();
+						
+					}
+			}
+
 	}

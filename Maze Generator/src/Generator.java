8203f056b72328057c861b87c8b74fdc05cc3c18
ethan.griffin <ethan.griffin@mu-c1-07.mullen.pvt.k12.co.us>
2019-09-04 12:22:26 -0600

    stareted tracer

--- Maze Generator/src/Generator.java
+++ Maze Generator/src/Generator.java
@@ -4,18 +4,64 @@ public class Generator
 
 		public static void main(String[] args)
 			{
+				
 				int mazeSize = 5;
 				Room[][] map = new Room[mazeSize][mazeSize];
 
+				
+				//fill map with rooms
 				for (int x = 0; x > mazeSize; x++)
 					{
 						for (int y = 0; y > mazeSize; y++)
 							{
 
-								map[x][y] = new Room();
+								map[x][y] = new Room(x,y);
 
 							}
 					}
+				
+				//create walls on outer boarder
+				for(Room[] collumn: map){
+					for(Room room: collumn){
+						//south walls
+						if(room.getxPos() == 0){
+							room.setDoor(0, "Closed");
+						}
+						
+						//East walls
+						if(room.getyPos() == mazeSize-1){
+							room.setDoor(1, "Closed");
+						}
+						
+						//North walls
+						if(room.getxPos() == mazeSize-1){
+							room.setDoor(2, "Closed");
+						}
+						
+						if(room.getyPos() == 0){
+							room.setDoor(3, "Closed");
+						}
+						
+						
+					}
+				}
+				
+				
+				
+				
+				
 			}
+		
+		
+		public static void traceMap(){
+			
+			
+			
+			
+		}
+		
 
+		
+		
+		
 	}
--- Maze Generator/src/Room.java
+++ Maze Generator/src/Room.java
@@ -4,11 +4,73 @@ public class Room
 
 		private int xPos;
 		private int yPos;
-		private String[] doors = new String[4];
+		private String[] doors;
 		private boolean hasBeenVisited;
-		
-		
-		
-		
-		
+		private String roomType;
+
+		public Room(int xPos, int yPos)
+			{
+				this.xPos = xPos;
+				this.yPos = yPos;
+				this.doors = new String[4];
+				this.hasBeenVisited = false;
+				this.roomType = "";
+
+			}
+
+		public int getxPos()
+			{
+				return xPos;
+			}
+
+		public void setxPos(int xPos)
+			{
+				this.xPos = xPos;
+			}
+
+		public int getyPos()
+			{
+				return yPos;
+			}
+
+		public void setyPos(int yPos)
+			{
+				this.yPos = yPos;
+			}
+
+		public String[] getDoors()
+			{
+				return doors;
+			}
+
+		public void setDoors(String[] doors)
+			{
+				this.doors = doors;
+			}
+
+		public void setDoor(int index, String doorState)
+			{
+				this.doors[index] = doorState;
+			}
+
+		public boolean isHasBeenVisited()
+			{
+				return hasBeenVisited;
+			}
+
+		public void setHasBeenVisited(boolean hasBeenVisited)
+			{
+				this.hasBeenVisited = hasBeenVisited;
+			}
+
+		public String getRoomType()
+			{
+				return roomType;
+			}
+
+		public void setRoomType(String roomType)
+			{
+				this.roomType = roomType;
+			}
+
 	}
